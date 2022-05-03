cc5905ff6eb436d636fb903fda9d3e445baab4a3
Jan Dreier <Dreier-96@web.de>
2022-05-03 14:47:26 +0200

    basic animation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatIconModule } from '@angular/material/icon'
+import {MatButtonModule} from '@angular/material/button';
+import {MatRippleModule} from '@angular/material/core';
+import {MatDialogModule} from '@angular/material/dialog';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StartScreenComponent } from './start-screen/start-screen.component';
+import { GameComponent } from './game/game.component';
+import { PlayerComponent } from './player/player.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DialogAddPlayerComponent } from './dialog-add-player/dialog-add-player.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StartScreenComponent,
+    GameComponent,
+    PlayerComponent,
+    DialogAddPlayerComponent
   ],
   imports: [
+    MatDialogModule,
+    MatRippleModule,
+    MatButtonModule,
+    MatIconModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
